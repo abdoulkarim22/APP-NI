3bbc9fbe27ef962576e71f32f79ca6fb7f593814
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-05-11 17:20:37 +0100

    add formulaire

--- components/Profile.js
+++ components/Profile.js
@@ -1,78 +1,105 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity,} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, ScrollView, TextInput, Button, TouchableWithoutFeedback, Alert } from 'react-native';
 
+const Profile = () => {
+  const [isSignUpModalVisible, setIsSignUpModalVisible] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const onclik = () => {
+    Alert.alert("Voleur Codeloccol");
+  };
 
-export default function Profile() {
+  const toggleSignUpModal = () => {
+    setIsSignUpModalVisible(!isSignUpModalVisible);
+  };
 
-  // const [username, setUsername] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [phoneNumber, setPhoneNumber] = useState('');
+  const handleModalPress = () => {
+    setIsSignUpModalVisible(false);
+  };
 
-  // const saveProfile = () => {
-  //   // Logique pour sauvegarder le profil (par exemple, enregistrement local)
-  //   console.log('Profil sauvegardé :', { username, email, phoneNumber });
+  const handleSignUp = () => {
+    if (!username || !email || !password) {
+      Alert.alert('Merci de remplir tous les champs.');
+    } else {
+      // Par exemple, appelez une fonction pour envoyer les données au backend
+      console.log('Creating account...');
+      console.log('Username:', username);
+      console.log('Email:', email);
+      console.log('Password:', password);
 
-  //   // Réinitialisation des champs après sauvegarde
-  //   setUsername('');
-  //   setEmail('');
-  //   setPhoneNumber('');
-  // };
+      // Fermez la modal après la création du compte
+      setIsSignUpModalVisible(false);
 
- 
+      // Réinitialisez les champs de texte
+      setUsername('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
   return (
     <ScrollView>
       <View style={styles.container}>
-       <Text>Log in to start planning your next trip</Text>
-       <View>
-          <TouchableOpacity>
-            <Text>
+        <Text style={{ fontSize: 15, fontWeight: '500' }}>Log in to start planning your next trip</Text>
+        <View style={styles.containertouchable}>
+          <TouchableOpacity onPress={onclik} style={styles.button}>
+            <Text style={styles.textbutton}>
               Login
             </Text>
           </TouchableOpacity>
-       </View>
-       <Text>Don't have a account ? </Text>
-        {/* <View style={{marginVertical:50}}>
-        <Image 
-           source = {require('../assets/Profile.jpeg')}
-           style={{width:100, height:100,}}
-       />
-          <Text style={styles.heading}>Mon Profil</Text>
-        </View> */}
-        {/* <TextInput
-          style={styles.input}
-          placeholder="Nom d'utilisateur"
-          value={username}
-          onChangeText={setUsername}
-          onPress={saveProfile}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          onPress={saveProfile}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Numéro de téléphone"
-          value={phoneNumber}
-          onChangeText={setPhoneNumber}
-          keyboardType="phone-pad"
-          onPress={saveProfile}
-        />
-        <Button title="Connecter" onPress={saveProfile} /> */}
-       
-
+          <Text style={{ marginTop: 12 }}>Don't have an account ?
+            <TouchableOpacity onPress={toggleSignUpModal}>
+              <Text style={styles.link}> Sign up</Text>
+            </TouchableOpacity>
+          </Text>
+        </View>
+        <Modal
+          visible={isSignUpModalVisible}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={toggleSignUpModal}
+        >
+          <TouchableWithoutFeedback onPress={handleModalPress}>
+            <View style={styles.modalContainer}>
+              <TouchableWithoutFeedback onPress={() => null}>
+                <View style={styles.modalContent}>
+                  <Text style={styles.modalHeading}>Create an Account</Text>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    value={email}
+                    onChangeText={setEmail}
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword}
+                  />
+                  <Button title="Sign Up" onPress={handleSignUp} />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
       </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding:20,
+    padding: 20,
     flex: 1,
   },
   heading: {
@@ -87,4 +114,42 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
   },
+  button: {
+    backgroundColor: 'green',
+    borderRadius: 5,
+    padding: 8,
+  },
+  textbutton: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: "center"
+  },
+  containertouchable: {
+    marginVertical: 30,
+  },
+  link: {
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    width: '80%',
+  },
+  modalHeading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
 });
+
+export default Profile;
