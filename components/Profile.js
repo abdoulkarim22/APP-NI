75431564ad77769c74102facd6f1af669edf71e3
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-05-11 15:54:55 +0100

    delete  swp

--- components/Profile.js
+++ components/Profile.js
@@ -1,61 +1,82 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet,ScrollView } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity,} from 'react-native';
+
+
 
 export default function Profile() {
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [phoneNumber, setPhoneNumber] = useState('');
 
-  const saveProfile = () => {
-    // Logique pour sauvegarder le profil (par exemple, enregistrement local)
-    console.log('Profil sauvegardé :', { username, email, phoneNumber });
-  };
+  // const saveProfile = () => {
+  //   // Logique pour sauvegarder le profil (par exemple, enregistrement local)
+  //   console.log('Profil sauvegardé :', { username, email, phoneNumber });
+
+  //   // Réinitialisation des champs après sauvegarde
+  //   setUsername('');
+  //   setEmail('');
+  //   setPhoneNumber('');
+  // };
+
+ 
   return (
     <ScrollView>
-      <View style={styles.container1}>
       <View style={styles.container}>
-      <Text style={styles.heading}>Mon Profil</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nom d'utilisateur"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Numéro de téléphone"
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        keyboardType="phone-pad"
-      />
-      <Button title="Connecter" onPress={saveProfile} />
-    </View>
-    </View>
+       <Text>Log in to start planning your next trip</Text>
+       <View>
+          <TouchableOpacity>
+            <Text>
+              Login
+            </Text>
+          </TouchableOpacity>
+       </View>
+       <Text>Don't have a account ? </Text>
+        {/* <View style={{marginVertical:50}}>
+        <Image 
+           source = {require('../assets/Profile.jpeg')}
+           style={{width:100, height:100,}}
+       />
+          <Text style={styles.heading}>Mon Profil</Text>
+        </View> */}
+        {/* <TextInput
+          style={styles.input}
+          placeholder="Nom d'utilisateur"
+          value={username}
+          onChangeText={setUsername}
+          onPress={saveProfile}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          onPress={saveProfile}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Numéro de téléphone"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          keyboardType="phone-pad"
+          onPress={saveProfile}
+        />
+        <Button title="Connecter" onPress={saveProfile} /> */}
+       
+
+      </View>
     </ScrollView>
   )
 }
- const styles = StyleSheet.create({
-  container1:{
+
+const styles = StyleSheet.create({
+  container: {
+    padding:20,
     flex: 1,
-    padding: 20,
-  }, 
-   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical:170,
   },
   heading: {
     fontSize: 24,
-    marginBottom: 20,
   },
   input: {
     width: '100%',
@@ -66,4 +87,4 @@ export default function Profile() {
     marginBottom: 10,
     paddingHorizontal: 10,
   },
- })
+});
