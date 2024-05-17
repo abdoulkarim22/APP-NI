edcb186103fe3ff25ac3a922a08054daa6443b24
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-05-17 14:46:57 +0100

    import text.....

--- components/Notifications.js
+++ components/Notifications.js
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Notifications() {
   return (
