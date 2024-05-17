741ba5317c92713be0b919b768645fbcf1aa6a46
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-05-17 14:47:42 +0100

    cree useState

--- components/Notifications.js
+++ components/Notifications.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Notifications() {
+
+  const [selectedTab, setSelectedTab] = useState('Messages');
   return (
     <View>
       <Text>Notifications</Text>
